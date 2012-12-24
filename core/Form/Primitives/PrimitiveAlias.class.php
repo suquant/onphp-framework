8b98956ee417a043094a2854d12c141394bb07cb
vasu <vasu.ru@ya.ru>
2012-12-25 03:35:54 +0400

    ordered methods

--- core/Form/Primitives/PrimitiveAlias.class.php
+++ core/Form/Primitives/PrimitiveAlias.class.php
@@ -15,140 +15,134 @@
 	final class PrimitiveAlias extends BasePrimitive
 	{
 		private $primitive = null;
-		
+
 		public function __construct($name, BasePrimitive $prm)
 		{
 			$this->name = $name;
 			$this->primitive = $prm;
 		}
-		
-		public function getInner()
-		{
-			return $this->primitive;
-		}
-		
-		public function getName()
-		{
-			return $this->name;
-		}
-		
-		public function getDefault()
-		{
-			return $this->primitive->getDefault();
-		}
-		
-		/**
-		 * @return PrimitiveAlias
-		**/
-		public function setDefault($default)
-		{
-			$this->primitive->setDefault($default);
-			
-			return $this;
-		}
-		
-		public function getValue()
-		{
-			return $this->primitive->getValue();
-		}
-		
-		public function getRawValue()
-		{
-			return $this->primitive->getRawValue();
-		}
-		
-		/**
-		 * @deprecated by getFormValue
-		**/
-		public function getActualValue()
-		{
-			if (null !== $this->primitive->getValue())
-				return $this->primitive->getValue();
-			elseif ($this->primitive->isImported())
-				return $this->primitive->getRawValue();
-			
-			return $this->primitive->getDefault();
-		}
-		
-		public function getSafeValue()
-		{
-			if ($this->primitive->isImported())
-				return $this->primitive->getValue();
-			
-			return $this->primitive->getDefault();
-		}
-		
-		public function getFormValue()
-		{
-			if (!$this->primitive->isImported()) {
-				if ($this->primitive->getValue() === null)
-					return null;
-				
-				return $this->primitive->exportValue();
-			}
-			
-			return $this->primitive->getRawValue();
-		}
-		
-		/**
-		 * @return PrimitiveAlias
-		**/
-		public function setValue($value)
-		{
-			$this->primitive->setValue($value);
-			
-			return $this;
-		}
-		
-		/**
-		 * @return PrimitiveAlias
-		**/
-		public function dropValue()
-		{
-			$this->primitive->dropValue();
-			
-			return $this;
-		}
-		
-		/**
-		 * @return PrimitiveAlias
-		**/
-		public function setRawValue($raw)
-		{
-			$this->primitive->setRawValue($raw);
-			
-			return $this;
-		}
-		
-		public function isImported()
-		{
-			return $this->primitive->isImported();
-		}
-		
+
 		/**
 		 * @return PrimitiveAlias
 		**/
 		public function clean()
 		{
 			$this->primitive->clean();
-			
+
 			return $this;
 		}
-		
-		public function importValue($value)
+
+		public function getInner()
 		{
-			return $this->primitive->importValue($value);
+			return $this->primitive;
 		}
-		
+
+		public function getName()
+		{
+			return $this->name;
+		}
+
+		public function getDefault()
+		{
+			return $this->primitive->getDefault();
+		}
+
+		/**
+		 * @return PrimitiveAlias
+		**/
+		public function setDefault($default)
+		{
+			$this->primitive->setDefault($default);
+
+			return $this;
+		}
+
+		/**
+		 * @return PrimitiveAlias
+		**/
+		public function setValue($value)
+		{
+			$this->primitive->setValue($value);
+
+			return $this;
+		}
+
+		public function getValue()
+		{
+			return $this->primitive->getValue();
+		}
+
+		/**
+		 * usually, you should not use this method
+		 *
+		 * @return PrimitiveAlias
+		**/
+		public function setRawValue($raw)
+		{
+			$this->primitive->setRawValue($raw);
+
+			return $this;
+		}
+
+		public function getRawValue()
+		{
+			return $this->primitive->getRawValue();
+		}
+
+		/**
+		 * @deprecated by getFormValue
+		**/
+		public function getActualValue()
+		{
+			return $this->primitive->getActualValue();
+		}
+
+		public function getSafeValue()
+		{
+			return $this->primitive->getSafeValue();
+		}
+
+		public function getFormValue()
+		{
+			if (!$this->primitive->isImported()) {
+				if ($this->primitive->getValue() === null)
+					return null;
+
+				return $this->primitive->exportValue();
+			}
+
+			return $this->primitive->getRawValue();
+		}
+
 		public function exportValue()
 		{
 			return $this->primitive->exportValue();
 		}
 
+		/**
+		 * @return PrimitiveAlias
+		**/
+		public function dropValue()
+		{
+			$this->primitive->dropValue();
+
+			return $this;
+		}
+
+
+		public function isImported()
+		{
+			return $this->primitive->isImported();
+		}
+
+		public function importValue($value)
+		{
+			return $this->primitive->importValue($value);
+		}
+
 		public function import($scope)
 		{
-			if (array_key_exists($this->name, $scope))
-			{
+			if (array_key_exists($this->name, $scope)) {
 				$result =
 					$this->primitive->import(
 						array(
@@ -163,12 +157,10 @@
 					$this->setError($error);
 				}
 
-
 				return $result;
 			}
 
-
 			return null;
 		}
+
 	}
-?>
